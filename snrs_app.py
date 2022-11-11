6657485d2f726de1d059b7602eaca3ae8729dae6
Thomas Cecconello <thomas.cecconello@unimib.it>
2022-11-11 17:42:32 +0000

    SSIM, Metrics, DoublePlot, small fix

--- snrs_app.py
+++ snrs_app.py
@@ -1,9 +1,12 @@
 from experiment import Experiment
 import numpy as np
+import itertools
 
 from skimage.metrics import structural_similarity as ssim
 
 import streamlit as st
+
+import time
 
 from PIL import Image
 
@@ -80,7 +83,7 @@ if check_analysis == 'Interactive':
             exp.clustering_hyp_param["K"] = st.slider('Number of cluster', key="K1", step = 1, min_value = 2, max_value = 20, value = 5)
 
         elif exp.clustering_hyp_param["method"] == "dbscan":
-            exp.clustering_hyp_param["eps"] = st.slider('Eps', key="eps1", step = 0.05, min_value = 0.0, max_value = 1.0, value = 0.1)
+            exp.clustering_hyp_param["eps"] = st.slider('Eps', key="eps1", step = 0.05, min_value = 0.05, max_value = 1.0, value = 0.1)
             exp.clustering_hyp_param["min_samples"] = st.slider('Min samples', key="min_samples1", step = 1, min_value = 2, max_value = 50, value = 5)
             
         elif exp.clustering_hyp_param["method"] == "hdbscan":
@@ -91,10 +94,10 @@ if check_analysis == 'Interactive':
 
         elif exp.clustering_hyp_param["method"] == 'agglomerative clustering':
             exp.clustering_hyp_param["n_clusters"] = st.slider('Number of clusters', key='n_clusters1', step =1, min_value = 2, max_value = 20, value = 5)
-        
+    
+    ssim_check = st.sidebar.checkbox("Perform SSIM", value=False)
     calculate_check = st.sidebar.button("Start compute interactive analysis")
     
-
 ### ------------------- Grid Search Reduction
 elif check_analysis == 'Grid search reduction':
     st.markdown("### 3.Choose Grid Search hyperparameters")
@@ -130,7 +133,7 @@ elif check_analysis == 'Grid search clustering':
             exp.clustering_grid_search_hyp_param["K"] = st.slider('Number of cluster', key="K2", step = 1, min_value = 2, max_value = 20, value = (3, 5))
 
         elif exp.clustering_grid_search_hyp_param["method"] == "dbscan":
-            exp.clustering_grid_search_hyp_param["eps"] = st.slider('Eps', key="eps", step = 0.05, min_value = 0.0, max_value = 1.0, value = (0.05, 0.5))
+            exp.clustering_grid_search_hyp_param["eps"] = st.slider('Eps', key="eps", step = 0.05, min_value = 0.05, max_value = 1.0, value = (0.05, 0.5))
             exp.clustering_grid_search_hyp_param["min_samples"] = st.slider('Min samples', key="min_samples2", step = 1, min_value = 2, max_value = 50, value = (2, 10))
             
         elif exp.clustering_grid_search_hyp_param["method"] == "hdbscan":
@@ -139,7 +142,7 @@ elif check_analysis == 'Grid search clustering':
         elif exp.clustering_grid_search_hyp_param["method"] == 'agglomerative clustering':
             exp.clustering_grid_search_hyp_param["n_clusters"] = st.slider('Number of clusters', key='n_clusters2', step =1, min_value = 2, max_value = 20, value = (2,8))
 
-    exp.clustering_grid_search_hyp_param["prereduction"]["n_neighbors"] = st.slider('(N2D) Number of neighbors', min_value = 5, max_value = 100, step = 5, value = 15)
+    exp.clustering_grid_search_hyp_param["prereduction"]["n_neighbors"] = st.slider('(N2D) Number of neighbors', min_value = 5, max_value = 100, step = 1, value = 8)
 
     calculate_grid_clus_check = st.button("Start compute grid clustering")
 
@@ -173,62 +176,72 @@ if check_analysis == 'Interactive':
         exp.load_experiment_data()
         exp.calculate_clusters()
         exp.calculate_reduction()
+        exp.calculate_clustering_metrics()
         exp.aggregate_data()
         exp.setup_bokeh_plot()
 
+        st.markdown("#### Silhouette: {} (Higher is better); &nbsp;&nbsp;Cal-Har: {} (Higher is better); &nbsp;&nbsp;Dav-Bou: {} (Lower is better)"
+            .format(
+                round(exp.metric_repr_silhouette, 2),
+                round(exp.metric_repr_calinski_harabasz, 2),
+                round(exp.metric_repr_davies_bouldin, 2)
+            )
+        )
+
         ### ------------------------ Visualization
         st.bokeh_chart(exp.bokeh_figure, use_container_width=True)
+        
+        if ssim_check:
+            ssim_list = []
+            # Per ogni cluster
+            for cluster in np.unique(exp.data_clusters):
+                cluster_rows = exp.aggregated_info[exp.aggregated_info.clusters == cluster]
+                ssim_sum = 0.
+                count = 0
 
-        ### ------------------------ Clusters
-        for cluster in np.unique(exp.data_clusters):
-            st.title('Cluster #' + str(cluster))
-            columns = st.columns(10)
-            count = 0
-            for _, row in exp.aggregated_info.iterrows():
-                if row.clusters == cluster:
-                    image = Image.open(row.gen_path)
-                    # n1 = np.array(image)
-                    
-                    # for _, row in exp.aggregated_info.iterrows():
-                    #     if row.clusters == cluster:
-                    #         image2 = Image.open(row.gen_path)
-                    #         n2 = np.array(image)
-                    #         ssim(n1,n2, channel_axis=2)
-                    
-                    with columns[count%10]:
-                        st.image(image, caption=row.image)
+                # Per coppia non ripetuta di immagini del cluster
+                for reference_image, compare_image in itertools.combinations(cluster_rows.gen_path, 2):
                     count+=1
-                        
+                    #print(reference_image)
+                    #print(compare_image)
+                    reference_image = np.array(Image.open(reference_image))
+                    compare_image = Image.open(compare_image)
+                    ssim_angle_max = 0.
+                    for angle in range(0,365,5):
+                        rotated_compare_image = np.array(compare_image.rotate(angle))
+                        ### DEBUG method
+                        # c1, c2, c3 = st.columns(3)
+                        # with c1:
+                        #     st.image(reference_image)
+                        # with c2:
+                        #     st.image(rotated_compare_image)
+                        # time.sleep(2)
+                        ssim_value = ssim(reference_image, rotated_compare_image, channel_axis=2)
+                        ssim_angle_max = max(ssim_value, ssim_angle_max)
+                        # with c3:
+                        #     st.write(round(ssim_value,2))
+                    #print(ssim_angle_max)
+                    ssim_sum = ssim_sum + ssim_angle_max
+                    #print(ssim_sum)
+                ssim_avg = ssim_sum / count
+                st.write(ssim_avg)
+                ssim_list.append(ssim_avg)
 
         ### ------------------------ Clusters
         for cluster in np.unique(exp.data_clusters):
-            st.title('Cluster #' + str(cluster))
+            if ssim_check:
+                st.title('Cluster #' + str(cluster) + " ssim:  " + str(round(ssim_list[cluster], 2)))
+            else:
+                st.title('Cluster #' + str(cluster))
             columns = st.columns(10)
             count = 0
             for _, row in exp.aggregated_info.iterrows():
                 if row.clusters == cluster:
-                    with columns[count%10]:
-                        image = Image.open(row.image_path)
-                        n1 = np.array(image)
-                        n2 = np.array(image.rotate(15))
-                        #np.moveaxis(na, 0, -1)
-                        st.write(ssim(n1,n2, channel_axis=2))
-                        st.image(image, caption=row.image)
-                        count+=1
-
-        # ref_image = #one of the images, e.g. G005.1+00.2.png
-
-        # new_image = #other random image
-
-        # angles = np.arange(0, 360, 5) # in steps of 5º
-
-        # new_img_PIL = Image.open(new_img)
-        # ssim_values = []
-        # for angle in angles:
-        #     rot = new_img_PIL.rotate(angle) # rotated image
-        #     save_path = PATH + f'tmp.png'   # output path
-        #     rot.save(PATH + f'tmp.png')     # save image
-
-        #     comp_img = save_path
-        #     ssim = out(f'pyssim --cw {ref_img} {comp_img}')
-        #     ssim_values.append(ssim)
+                    image_input = Image.open(row.image_path)
+                    image_gen = Image.open(row.gen_path)
+                    
+                    with columns[count%5]:
+                        st.image(image_gen, caption=row.image)
+                    with columns[(count%5)+5]:
+                        st.image(image_input, caption=row.image)
+                    count+=1
